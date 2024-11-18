8368b9550d2c93639dd8ad46d5914753fbbdc7ec
¨Shailashri <magadumshaila@gmail.com>
2024-11-18 18:39:32 +0100

    deleted file

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
   watchForFileChanges:false,
+  projectId: "9n8wr4",
   //viewportHeight:736,
   //viewportWidth:414, //iphone 6 screen we will get the output
   e2e: {
-    specPattern: "**/*.feature",
+    //specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
       // implement node event listeners here
---
+++ cypress/e2e/Tests/Testlamdatest.feature
@@ -0,0 +1,4 @@
+# https://ecommerce-playground.lambdatest.io/
+Feature:Validate the home page
+
+we will validate the home page of ure Description
